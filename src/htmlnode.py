1608b53541cfa3cd4551e1ec938fa1afdcc01086
aruiz <aruiz4466@gmail.com>
2026-04-14 01:59:33 -0700

    added ParentNode class to HTMLNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,15 +21,31 @@ class HTMLNode:
     
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, None,props)
+        super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode value is None")
+            raise ValueError("invalid HTML: no value")
         if self.tag is None:
-            return f"{self.value}"
-        html_prop = self.props_to_html()
-        return f"<{self.tag}{html_prop}>{self.value}</{self.tag}>"
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        inner_html = ""
+        for child in self.children:
+            inner_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{inner_html}</{self.tag}>"
+        
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
