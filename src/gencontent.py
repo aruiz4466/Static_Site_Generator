b4c571fa3923f34fb76dacf48b35f65a9a63eb59
aruiz <aruiz4466@gmail.com>
2026-05-03 22:44:04 -0700

    Deploy site to GitHub Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,7 +3,7 @@ import pathlib
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -19,6 +19,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -35,13 +37,13 @@ def extract_title(md):
     raise ValueError("no title found")
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for files in os.listdir(dir_path_content):
         dir_path_files = os.path.join(dir_path_content, files)
         dest_dir_files = os.path.join(dest_dir_path, files)
         if os.path.isfile(dir_path_files):
             dest_dir_files = pathlib.Path(dest_dir_files)
             dest_dir_files = dest_dir_files.with_suffix(".html")
-            generate_page(dir_path_files, template_path, dest_dir_files)
+            generate_page(dir_path_files, template_path, dest_dir_files, basepath)
         else:
-            generate_pages_recursive(dir_path_files, template_path, dest_dir_files)
+            generate_pages_recursive(dir_path_files, template_path, dest_dir_files, basepath)
